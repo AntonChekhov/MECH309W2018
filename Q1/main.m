76bf6de1c89df39c63a2666135232cc3c49c3b3c
root <root@vassili-Satellite-C40-B>
2018-03-26 11:33:32 -0400

    Change out of domain values to missing (from magic value of -100000) Add missing values for tau's whose temperature coefficients are undefined for a set of points. Flatten resulting matrices and add/solve system of equations.

--- Q1/main.m
+++ Q1/main.m
@@ -36,12 +36,13 @@ for i = 1:n+1
     for j = 1:n+1
         %Monkey values for points outside considered domain
         if i > n/2 && j > n/2
-            Tau_ij(i,j) = -1000000;
-            Tau_ip(i,j) = -1000000;
-            Tau_im(i,j) = -1000000;
-            Tau_jp(i,j) = -1000000;
-            Tau_jm(i,j) = -1000000;
-            B_ij(i,j) = -1000000;
+            Tau_ij(i,j) = missing;
+            Tau_ip(i,j) = missing;
+            Tau_im(i,j) = missing;
+            Tau_jp(i,j) = missing;
+            Tau_jm(i,j) = missing;
+            B_ij(i,j) = missing;
+            T_ij(i,j) = missing;
         end 
         
         %BC's - temperature zero all sides
@@ -56,9 +57,68 @@ for i = 1:n+1
                 Tau_jp(i,j) = 0;
                 Tau_jm(i,j) = 0;
                 B_ij(i,j) = 0;
-                    
         end
         
+        %Take away coefficients for boundaries where their corresponding
+        %temperature term is undefined - e.g. Tau_im is undefined when i =
+        %0
+        if i == 1 
+            Tau_im(i,j) = 0; %These go into the matrix. 
+        end 
+        
+        if j == 1 
+            Tau_jm(i,j) = missing;
+        end 
+        
+        if (i == n+1 && j <= n/2) || (i == n/2 && j >= n/2 )
+            Tau_ip(i,j) = 0;
+        end 
+        
+        if (i <= n/2 && j == n+1) || (i > n/2 && j == n/2) 
+            Tau_jp(i,j) = missing;
+        end 
+        
+
+        
+
     end
 end
+
+%% Flatten created matrices to vectors 
+%vectors are lowercase!!
+%A(:) yields column concantenation of originals. A'(:) doesnt get accepted
+%by MATLAB, so use temporary matrices. 
+%Naming is unfortunate. LOWERCASE are flattened vectors. Capitalized are
+%matrices. 
+Tau_ij_t = Tau_ij'; tau_ij = Tau_ij(:); 
+Tau_ip_t = Tau_ip'; tau_ip = Tau_ip_t(:);
+Tau_im_t = Tau_im'; tau_im = Tau_im(:);
+Tau_jp_t = Tau_jp'; tau_jp = Tau_jp(:);
+Tau_jm_t = Tau_jm'; tau_jm = Tau_jm(:);
+B_ij_t = B_ij';     b_ij = B_ij(:);
+T_ij_t = B_ij';     t_ij = B_ij(:);
+
+clearvars -except tau_ij tau_ip tau_im tau_jp tau_jm b_ij t_ij n %clean up workspace cause it be messy
+
+%Remove points outside boundaries from created vectors
+tau_ij = tau_ij(~ismissing(tau_ij));
+ 
+tau_ip = tau_ip(~ismissing(tau_ip)); tau_ip = tau_ip(1:(length(tau_ip)-1));
+tau_im = tau_im(~ismissing(tau_im)); tau_im = tau_im(2:length(tau_im));
+
+tau_jp = tau_jp(~ismissing(tau_jp));
+tau_jm = tau_jm(~ismissing(tau_jm));
+
+b_ij = b_ij(~ismissing(b_ij));
+
+%% Construct and solve system
+A = diag(tau_ij, 0)+diag(tau_im, -1) + diag(tau_ip,1) + ...
+diag(tau_jm, n+1) + diag(tau_jm, -(n+1));
+b = b_ij;
+t = gaussianElim(A,b);
+
+%% Reconstruct 2D domain for plotting
+
+
+
 

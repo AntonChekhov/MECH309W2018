101c33d3f75be0d897d1740925216396e4606d77
AntonChekhov <vkorotkin@gmail.com>
2018-04-06 16:38:34 -0400

    Convergence plot fix

--- Q1/Convergence.m
+++ Q1/Convergence.m
@@ -3,11 +3,15 @@ loglog(nArray, ...
     abs(convergenceArr-convergenceArr(length(convergenceArr))))
 % plot(nArray, ...
 %     abs(convergenceArr-convergenceArr(length(convergenceArr))))
-
+hold on 
+loglog(nArray) 
 
 nTest = log(nArray);
 convTest = log(abs(convergenceArr-convergenceArr(length(convergenceArr))));
+compSeries = arrayfun(@(n) 1/n^1.7, nArray); compSeries = log(compSeries)-5;
 
 figure
 plot(nTest, convTest);
-convTest(16)/nTest(16)
+hold on
+plot(nTest, compSeries);
+legend('Convergence Error', '1/n^{1.7}')
